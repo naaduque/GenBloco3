c4bcd0e8a354f7bc8cd4ba40921554ae2988093c
Natalia <naaduque@gmail.com>
2022-02-12 00:16:29 -0300

    Atualização zoom mapa

--- Entregável  - Fundamentos Web/assets/js/script.js
+++ Entregável  - Fundamentos Web/assets/js/script.js
@@ -12,6 +12,10 @@ let nome = window.document.getElementById("nome")
 let email = document.querySelector("#email")
 let assunto = document.querySelector("#assunto")
 let data = document.querySelector("#data")
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+let mapa = document.querySelector("#mapa")
 
 nome.style.width = "100%"
 email.style.width = "100%"
@@ -25,6 +29,7 @@ function validaNome() {
      } else {
          txtNome.innerHTML = "Nome válido"
          txtNome.style.color = "green"
+         nomeOk = true 
      }
 
 }
@@ -38,17 +43,37 @@ function validaEmail() {
     } else {
         txtEmail.innerHTML = "E-mail válido"
         txtEmail.style.color = "green"
+        emailOk = true
     }
     
 }
 
 function validaAssunto() {
     let txtAssunto = document.querySelector("#txtAssunto")
-    if (assunto.value.length >=150) {
+    if (assunto.value.length >= 150) {
         txtAssunto.innerHTML = "Texto muito grande, digite no máximo 150 caracteres"
         txtAssunto.style.color = "red"
         txtAssunto.style.display = "block"
     } else {
         txtAssunto.style.display = "none"
+        assuntoOk = true
     }
+}
+
+function enviar() {
+    if (nomeOk == true && emailOk == true && assuntoOk == true) {
+        alert ("Formulário enviado com sucesso!")
+    } else {
+        alert ("Preencha o formulário corretamente antes de enviar")
+    }
+}
+
+function mapaZoom() {
+    mapa.style.width = "800px"
+    mapa.style.height = "600px"
+}
+
+function mapaNormal() {
+    mapa.style.width = "500px"
+    mapa.style.height = "320px"
 }
